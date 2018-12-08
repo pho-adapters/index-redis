43f9d803e53e7b2ca2428d48a3300e69171487aa
Emre Sokullu <esokullu@gmail.com>
2018-12-07 18:43:16 -0800

    Update QueryResult.php

--- QueryResult.php
+++ QueryResult.php
@@ -27,12 +27,16 @@ class QueryResult extends \Pho\Kernel\Services\Index\QueryResult
     {
         error_log("Redis QueryResult executing");
          error_log("Resuts is a: ".get_class($results));
-         //error_log("Resuts are: ".print_r($results->values, true));
-
+        error_log("Resuts are: ".print_r($results, true));
+        error_log("Resuts are: ".print_r($results->values, true));
+        
+/*
        foreach($results->values as $result) // $result would be a \GraphAware\Bolt\Result\Result 
        {
            $this->results[] = array_values($result);
        }
+       */
+        $this->results = $results->values;
        $stats = $results->stats;
        $this->summary["nodesCreated"] = isset($stats["nodes_created"])?$stats["nodes_created"]:0;
        $this->summary["nodesDeleted"] = isset($stats["nodes_deleted"])?$stats["nodes_deleted"]:0;
